33b65be226b9c5226c0410b44f46bfaf52d69ba9
Eray Genç <eraygencodev@hotmail.com>
2025-11-25 00:22:41 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import LoginPage from './pages/LoginPage'
+import  '../src/App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <LoginPage/>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/auth/LoginForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { TextField } from '@mui/material';
+
+function LoginForm() {
+  return (
+    
+    <div className='login-form-back'>
+        <h1>SaaS for Visualization</h1>
+        <h2>Hoş Geldin...</h2>
+        <p>Veri Görselleştirme için lütfen giriş sağlayın<br />  eğer kaydınız yoksa aşağıdaki linkten<br /> kayıt oluşturabilirsiniz.</p>
+    
+
+    </div>
+    
+    
+  )
+}
+
+export default LoginForm
